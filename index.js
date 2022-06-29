24304278d5c5543a59431986689d6b4de0c70e2c
Andrianna <andrianna.odell@gmail.com>
2022-06-29 17:36:16 -0400

    commit tree 70c2505c25d63f956df0d9c5e6d6ec69441c2aa9

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Your code here
+
+// save a reference of the dodger to a variable
+document.addEventListener("keydown", function (event) {
+    if(event.key === "ArrowLeft"){
+        moveDodgerLeft();
+    }
+});
+
+document.addEventListener("keydown", function (event) {
+    if(event.key === "ArrowRight"){
+        dodger = document.getElementById('dodger')
+        moveDodgerRight();
+    }
+});
+
+// move the dodger to the left
+function moveDodgerLeft(){
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+    if(left > 0){
+        dodger.style.left = `${left - 1}px`;
+    }
+}
+
+function moveDodgerRight(){
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+
+    if(left > 0){
+        dodger.style.left = `${left + 1}px`;
+    }
+}
